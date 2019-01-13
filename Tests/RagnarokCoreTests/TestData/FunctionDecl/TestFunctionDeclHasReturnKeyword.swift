7bee1580e3594c971ec03e67f52b67f3bf470846
bannzai <kingkong999yhirose@gmail.com>
2019-01-13 18:57:35 +0900

    :sparkles: Using optional value

--- Tests/RagnarokCoreTests/TestData/FunctionDecl/TestFunctionDeclHasReturnKeyword.swift
+++ Tests/RagnarokCoreTests/TestData/FunctionDecl/TestFunctionDeclHasReturnKeyword.swift
@@ -4,13 +4,13 @@ public class TestFunctionDeclHasReturnType: TestDatable {
     public static func file() -> String {
         return #file
     }
-    func noArgumentHasReturnKeyword() -> Int {
+    func noArgumentHasReturnKeyword() -> Int? {
         return 1
     }
-    func oneArgumentHasReturnKeyword(argument: Int) -> Int {
+    func oneArgumentHasReturnKeyword(argument: Int) -> Int? {
         return 1
     }
-    func twoArgumentHasReturnKeyword(argument1: Int, argument2: String) -> Int {
+    func twoArgumentHasReturnKeyword(argument1: Int, argument2: String) -> Int? {
         return 1
     }
 }
