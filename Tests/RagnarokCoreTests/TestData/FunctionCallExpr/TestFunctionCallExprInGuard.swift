c673c3f99cd4448fc07c3e520706284014d63b38
bannzai <kingkong999yhirose@gmail.com>
2019-01-13 16:28:32 +0900

    :sparkles: add expected method

--- Tests/RagnarokCoreTests/TestData/FunctionCallExpr/TestFunctionCallExprInGuard.swift
+++ Tests/RagnarokCoreTests/TestData/FunctionCallExpr/TestFunctionCallExprInGuard.swift
@@ -4,6 +4,29 @@ public class TestFunctionCallExprInGuard: TestDatable {
     public static func file() -> String {
         return #file
     }
+    public static func expected() -> String {
+        let expected = """
+import Foundation
+
+public class TestFunctionCallExprNoReturn: TestDatable {
+    public static func file() -> String {
+        return #file
+    }
+    public func example() {
+        let test = TestFunctionDeclNoReturn()
+        test.noArgumentNoReturn()
+        test.oneArgumentNoReturn(argument: 1)
+        test.twoArgumentNoReturn(
+            argument1: 1,
+            argument2: "2"
+        )
+    }
+}
+
+"""
+        return expected
+    }
+
     public func example() {
         let test = TestFunctionDeclHasReturnType()
         
--- Tests/RagnarokCoreTests/TestData/FunctionCallExpr/TestFunctionCallExprNoReturn.swift
+++ Tests/RagnarokCoreTests/TestData/FunctionCallExpr/TestFunctionCallExprNoReturn.swift
@@ -4,6 +4,27 @@ public class TestFunctionCallExprNoReturn: TestDatable {
     public static func file() -> String {
         return #file
     }
+    public static func expected() -> String {
+        return """
+import Foundation
+
+public class TestFunctionCallExprNoReturn: TestDatable {
+    public static func file() -> String {
+        return #file
+    }
+    public func example() {
+        let test = TestFunctionDeclNoReturn()
+        test.noArgumentNoReturn()
+        test.oneArgumentNoReturn(argument: 1)
+        test.twoArgumentNoReturn(
+            argument1: 1,
+            argument2: "2"
+        )
+    }
+}
+
+"""
+    }
     
     public func example() {
         let test = TestFunctionDeclNoReturn()
