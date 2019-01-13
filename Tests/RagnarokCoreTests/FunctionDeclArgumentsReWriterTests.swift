79e574b80cde9372af2a5db0c622c7008b278bca
bannzai <kingkong999yhirose@gmail.com>
2019-01-13 15:47:06 +0900

    :tada: Fix test case of TestFunctionCallExprNoReturn

--- Tests/RagnarokCoreTests/FunctionDeclArgumentsReWriterTests.swift
+++ Tests/RagnarokCoreTests/FunctionDeclArgumentsReWriterTests.swift
@@ -3,9 +3,32 @@ import class Foundation.Bundle
 @testable import RagnarokCore
 
 final class FunctionDeclArgumentsReWriterTests: XCTestCase {
-    func testFormatted() {
-        XCTContext.runActivity(named: "Test visit of FunctionCallExprSyntaxx") { (activity) in
-//            let input =
+    func testFormatted() throws {
+        try XCTContext.runActivity(named: "Test visit of FunctionCallExprSyntax") { (activity) in
+            try XCTContext.runActivity(named: "Test visit of TestFunctionCallExprNoReturn") { (activity) in
+                let input = URL(string: TestFunctionCallExprNoReturn.file())!
+                let expected = """
+import Foundation
+
+public class TestFunctionCallExprNoReturn: TestDatable {
+    public static func file() -> String {
+        return #file
+    }
+    func example() {
+        let test = TestFunctionDeclNoReturn()
+        test.noArgumentNoReturn()
+        test.oneArgumentNoReturn(argument: 1)
+        test.twoArgumentNoReturn(
+            argument1: 1,
+            argument2: "2"
+        )
+    }
+}
+
+"""
+                let rewriter = try FunctionDeclArgumentsReWriter(path: input)
+                XCTAssertEqual(try rewriter.formatted(), expected)
+            }
         }
     }
 }
