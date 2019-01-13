3dd95aae10402e460d8cacfc93680f0da93fd58d
bannzai <kingkong999yhirose@gmail.com>
2019-01-13 16:37:13 +0900

    :tada: Fix test case of TestFunctionCallExprInGuard

--- Tests/RagnarokCoreTests/FunctionDeclArgumentsReWriterTests.swift
+++ Tests/RagnarokCoreTests/FunctionDeclArgumentsReWriterTests.swift
@@ -29,6 +29,44 @@ public class TestFunctionCallExprNoReturn: TestDatable {
                 let rewriter = try FunctionDeclArgumentsReWriter(path: input)
                 XCTAssertEqual(try rewriter.formatted(), expected)
             }
+            
+            try XCTContext.runActivity(named: "Test visit of TestFunctionCallExprInGuard") { (activity) in
+                let input = URL(string: TestFunctionCallExprInGuard.file())!
+                let expected = """
+import Foundation
+
+public class TestFunctionCallExprInGuard: TestDatable {
+    public static func file() -> String {
+        return #file
+    }
+    func example() {
+        let test = TestFunctionDeclHasReturnType()
+        
+        guard let a = Optional(test.noArgumentHasReturnKeyword()) else {
+            return
+        }
+        guard let b = Optional(test.oneArgumentHasReturnKeyword(argument: 1)) else {
+            return
+        }
+        guard let c = Optional(test.twoArgumentHasReturnKeyword(
+            argument1: 1,
+            argument2: "string"
+        )) else {
+            return
+        }
+        
+        print(
+            a,
+            b,
+            c
+        )
+    }
+}
+
+"""
+                let rewriter = try FunctionDeclArgumentsReWriter(path: input)
+                XCTAssertEqual(try rewriter.formatted(), expected)
+            }
         }
     }
 }
