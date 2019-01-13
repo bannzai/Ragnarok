685815512ad10e7bc3e8cdf4c6dc4521b7496f2d
bannzai <kingkong999yhirose@gmail.com>
2019-01-13 22:29:34 +0900

    :sparkles: Add RagnarokRewriter

---
+++ Sources/RagnarokCore/RagnarokRewriter.swift
@@ -0,0 +1,13 @@
+//
+//  RagnarokRewriter.swift
+//  RagnarokCore
+//
+//  Created by Yudai.Hirose on 2019/01/13.
+//
+
+import Foundation
+
+public class RagnarokRewriter: SyntaxRewriter {
+    
+}
+
