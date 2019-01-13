f1b7e964476135839acb1a710bbbfde859cf6c9b
bannzai <kingkong999yhirose@gmail.com>
2019-01-13 15:35:50 +0900

    :sparkles: Fix test case

--- Tests/RagnarokCoreTests/TestData/TestFunctionCallExprInIf.swift
+++ Tests/RagnarokCoreTests/TestData/TestFunctionCallExprInIf.swift
@@ -10,13 +10,13 @@ import Foundation
 public class TestFunctionCallExprInIf {
     func example() {
         let test = TestFunctionDeclHasReturnType()
-        if let a = test.noArgumentHasReturnKeyword() {
+        if let a = Optional(test.noArgumentHasReturnKeyword()) {
             print(a)
         }
-        if let b = test.oneArgumentHasReturnKeyword(argument: 1) {
+        if let b = Optional(test.oneArgumentHasReturnKeyword(argument: 1)) {
             print(b)
         }
-        if let c = test.twoArgumentHasReturnKeyword(argument1: 1, argument2: "string") {
+        if let c = Optional(test.twoArgumentHasReturnKeyword(argument1: 1, argument2: "string")) {
             print(c)
         }
     }
