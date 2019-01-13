52458fbef34b78a951251a8369408c1cc94f5e0a
bannzai <kingkong999yhirose@gmail.com>
2019-01-13 23:26:14 +0900

    :recycle: Fix expected

--- Tests/RagnarokCoreTests/RagnarokRewriterTests.swift
+++ Tests/RagnarokCoreTests/RagnarokRewriterTests.swift
@@ -334,17 +334,17 @@ public class TestFunctionDeclMultipleLabel: TestDatable {
                 let expected = """
 import Foundation
 
-public class TestFunctionDeclMultipleLabel: TestDatable {
+public class TestFunctionDeclDiscardLabel: TestDatable {
     public static func file() -> String {
         return #file
     }
     
-    func oneArgument(label1 argument1: Int) {
+    func oneArgument(_ argument1: Int) {
         
     }
     func multipleArgument(
-        label1 argument1: Int,
-        label2 argument2: String
+        _ argument1: Int,
+        _ argument2: String
         ) {
         
     }
