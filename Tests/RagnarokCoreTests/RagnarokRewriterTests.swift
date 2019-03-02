07c7525d12b61d50a6db39d7303feb6c440f1f41
bannzai <kingkong999yhirose@gmail.com>
2019-03-02 23:13:14 +0900

    :sparkles: Add test case for already multiple line

--- Tests/RagnarokCoreTests/RagnarokRewriterTests.swift
+++ Tests/RagnarokCoreTests/RagnarokRewriterTests.swift
@@ -406,6 +406,43 @@ public class TestFunctionDeclHasEllipsis: TestDatable {
                 let rewriter = try RagnarokRewriter(path: input)
                 XCTAssertEqual(try rewriter.formatted(), expected)
             }
+            
+            try XCTContext.runActivity(named: "Test visit of TestFunctionDeclForAlreadyMultipleLine") { (activity) in
+                let input = URL(string: TestFunctionDeclForAlreadyMultipleLine.file())!
+                let expected = """
+import Foundation
+
+public class TestFunctionDeclForAlreadyMultipleLine: TestDatable {
+    public static func file() -> String {
+        return #file
+    }
+    func closureNoEscaping(
+        closure: () -> Void
+        ) {
+        
+    }
+    func closureEscaping(
+        closure: @escaping () -> Void
+        ) {
+        
+    }
+    func twoClosureNoEscaping(
+        closure1: () -> Void,
+        closure2: () -> Void
+        ) {
+        
+    }
+    func twoClosureEscaping(
+        closure1: @escaping () -> Void,
+        closure2: @escaping () -> Void
+        ) {
+        
+    }
+}
+"""
+                let rewriter = try RagnarokRewriter(path: input)
+                XCTAssertEqual(try rewriter.formatted(), expected)
+            }
 
 
         }
